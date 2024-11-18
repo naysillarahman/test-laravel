226660254e15bf663728b598f2aed27b69741512
Rahadirahman21 <rahadi.rahman2007@smkn4-tsm.sch.id>
2024-11-18 10:14:56 +0700

    menambahkan project

---
+++ resources/views/auth/layouts.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="widht=device-widht, initial-scale=1.0">
+    <mate http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Laravel 11 </title>
+</head>
+
+<body>
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+</body>
+
+</html>
